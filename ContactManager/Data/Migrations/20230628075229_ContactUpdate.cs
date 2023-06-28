56c6f59778952455b847b68bd2cf75b54849728f
dimitar_georgiev <dimitar.georgiev99@abv.bg>
2023-06-28 13:05:09 +0300

    Add project files.

---
+++ ContactManager/Data/Migrations/20230628075229_ContactUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContactUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OwnewrID",
+                table: "Contact",
+                newName: "OwnerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OwnerID",
+                table: "Contact",
+                newName: "OwnewrID");
+        }
+    }
+}
